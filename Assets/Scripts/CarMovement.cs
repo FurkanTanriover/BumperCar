f8566189ef6da86a2bd50ac19367c5da9402e9ce
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2021-05-08 16:37:37 +0300

    Project Added

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+
+    public float moveSpeed;
+    public float gravity = -10f;
+
+
+
+    Vector3 velocity;
+ 
+
+    void Update()
+    { 
+      //  float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * z;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
